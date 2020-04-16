4586170cdacb428151cac1e80ecc519a14403bfc
MaticNezic <matic.nezic@celtra.com>
2020-04-16 19:42:19 +0200

    Added routes for POST and DELETE actors

--- projects/capstone/starter/app.py
+++ projects/capstone/starter/app.py
@@ -12,15 +12,66 @@ def create_app(test_config=None):
   setup_db(app)
   CORS(app, resources={r"*": {"origins": "*"}})
 
+  ''' ACTORS ROUTES '''
+  ''' GET all /actors'''
   @app.route('/actors', methods=['GET'])
-  def index():
+  def get_actors():
+      try:
         query = Actor.query.all()
+
+        if not query:
+            abort(404)
 
         return jsonify({
           'status': 200,
           'success': True,
-          'actors': [i.format() for i in query]
+          'actors': [i.format() for i in query],
+          'total_actors': len(query)
         })
+      except:
+          abort(500)
+
+  ''' POST /actors'''
+  @app.route('/actors', methods=['POST'])
+  def post_actors():
+      try:
+        body = request.get_json()
+        if not body:
+            abort(400)
+
+        actor = Actor(name=body['name'], age=body['age'], gender=body['gender'])
+        actor.insert()
+        query = Actor.query.all()
+
+        return jsonify({
+          'status': 201,
+          'success': True,
+          'actor': actor.format(),
+          'total_actors': len(query)
+        })
+      except:
+          abort(400)
+
+  ''' DELETE /actors'''
+  @app.route('/actors/<int:actor_id>', methods=['DELETE'])
+  def delete_actors(actor_id):
+      try:
+        actor = Actor.query.filter(
+            Actor.id == actor_id).one_or_none()
+        if actor_id is None:
+            abort(404)
+        else:
+            actor.delete()
+            query = Actor.query.all()
+
+            return jsonify({
+                'status': 200,
+                'success': True,
+                'deleted': actor_id,
+                'total_questions': len(query)
+            })
+      except:
+          abort(422)
 
   return app
 
